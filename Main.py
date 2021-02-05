b82c8b941c6741c62ddac073a41708e46ffaf88c
nitishsadire <nitishsandhu9@gmail.com>
2021-02-05 23:07:31 +0530

    corrections

--- Main.py
+++ Main.py
@@ -186,6 +186,7 @@ def train_model(unet, optimizer, scheduler, dataloader, dataset_sizes, device, l
             print(outputs.shape)
 
             _, preds = torch.max(outputs, 1)
+            print(preds.shape)
             plt.imshow(ground_masks_to_colorimg(preds.unsqueeze(0)) / 255.)
             plt.show()
 
