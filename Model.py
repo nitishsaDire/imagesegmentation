0575f6297a4bc24bec0fbc68edce814ff38e918c
nitishsadire <nitishsandhu9@gmail.com>
2021-02-06 16:07:06 +0530

    changes

--- Model.py
+++ Model.py
@@ -42,7 +42,7 @@ class UNET_resnet34(nn.Module):
         # print(self.conv2d(512, 256)(self.upsample(x5e)).shape)
         upp = self.upsample(x5e)
         catt = torch.cat((x4e, self.conv2d(512, 256, kernel=3, padding=1)(upp)), 1)
-        print(catt.shape, upp.shape)
+        print(x4e.shape, catt.shape, upp.shape)
         x4d = self.conv2d(512, 256, kernel=3, padding=1)(catt)            #14x14,    256
         x3d = self.conv2d(256, 128, kernel=3, padding=1)(torch.cat((x3e, self.conv2d(256, 128, kernel=3, padding=1)(self.upsample(x4d))), 1))            #28x28,    128
         x2d = self.conv2d(128, 64, kernel=3, padding=1)(torch.cat((x2e, self.conv2d(128,  64, kernel=3, padding=1)(self.upsample(x3d))), 1))              #56x56,    64
