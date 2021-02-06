9d818b663374e1acc26a508ae080b7144ec6f222
nitishsadire <nitishsandhu9@gmail.com>
2021-02-06 16:03:27 +0530

    changes

--- Model.py
+++ Model.py
@@ -12,7 +12,7 @@ class UNET_resnet34(nn.Module):
         self.dropout_p = dropout_p
         self.resnet = models.resnet34(pretrained=True)
         self.resnet_layers = list(self.resnet.children())
-        self.upsample = nn.Upsample(scale_factor=2, mode='nearest')
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear')
         self.softmax = nn.Softmax(dim=1)
         use_cuda = torch.cuda.is_available()  # check if GPU exists
         self.device = torch.device("cuda" if use_cuda else "cpu")  # use CPU or GPU
@@ -21,7 +21,7 @@ class UNET_resnet34(nn.Module):
         return nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=kernel, stride=stride, padding = padding),
             nn.BatchNorm2d(out_channels),
-            nn.Dropout2d(p=self.dropout_p),
+            # nn.Dropout2d(p=self.dropout_p),
             nn.ReLU()).to(self.device)
 
     def forward(self, x):
@@ -40,10 +40,12 @@ class UNET_resnet34(nn.Module):
         # print(x5e.shape)
         # print(torch.cat((x4e, self.conv2d(512, 256)(self.upsample(x5e))), 1).shape)
         # print(self.conv2d(512, 256)(self.upsample(x5e)).shape)
-
-        x4d = self.conv2d(512, 256, kernel=3, padding=1)(torch.cat((x4e, self.conv2d(512, 256)(self.upsample(x5e.to(self.device)))), 1))            #14x14,    256
-        x3d = self.conv2d(256, 128, kernel=3, padding=1)(torch.cat((x3e, self.conv2d(256, 128)(self.upsample(x4d))), 1))            #28x28,    128
-        x2d = self.conv2d(128, 64, kernel=3, padding=1)(torch.cat((x2e, self.conv2d(128,  64)(self.upsample(x3d))), 1))              #56x56,    64
+        upp = self.upsample(x5e)
+        catt = torch.cat((x4e, self.conv2d(512, 256, kernel=3, padding=1)(upp)), 1)
+        print(catt.shape, upp.shape)
+        x4d = self.conv2d(512, 256, kernel=3, padding=1)(catt)            #14x14,    256
+        x3d = self.conv2d(256, 128, kernel=3, padding=1)(torch.cat((x3e, self.conv2d(256, 128, kernel=3, padding=1)(self.upsample(x4d))), 1))            #28x28,    128
+        x2d = self.conv2d(128, 64, kernel=3, padding=1)(torch.cat((x2e, self.conv2d(128,  64, kernel=3, padding=1)(self.upsample(x3d))), 1))              #56x56,    64
         x1d = self.conv2d(128, 64, kernel=3, padding=1)(torch.cat((x1e, self.upsample(x2d)), 1))                                     #112x112,  64
 
         final = self.conv2d(64, self.n_classes)(self.upsample(x1d))
