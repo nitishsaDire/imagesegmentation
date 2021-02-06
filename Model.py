c6129839a3537206eb45597bb31414cbdc9419c6
nitishsadire <nitishsandhu9@gmail.com>
2021-02-06 20:42:37 +0530

    changes

--- Model.py
+++ Model.py
@@ -29,7 +29,8 @@ class UNET_resnet34(nn.Module):
             nn.Conv2d(in_channels, out_channels, kernel_size=kernel, stride=stride, padding = padding),
             # nn.BatchNorm2d(out_channels),
             # nn.Dropout2d(p=self.dropout_p),
-            nn.ReLU()).to(self.device)
+            # nn.ReLU()
+        ).to(self.device)
 
     def forward(self, x):
         """
