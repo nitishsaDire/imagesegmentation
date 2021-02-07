f94a5d6890db2c7999866ec3713772c55d98fe07
nitishsadire <nitishsandhu9@gmail.com>
2021-02-07 13:28:29 +0530

    changes

--- Model.py
+++ Model.py
@@ -67,6 +67,7 @@ class UNET_resnet34(nn.Module):
         x5e = self.seq5(x4e)        # 7x7, 512
         x6e = self.conv2d_0(x5e)    # 3x3, 1024
         x7e = self.conv2d_01(x6e)   # 1x1, 2048
+        print(x7e.shape)
 
         x6d = self.conv2d_0d(torch.cat((x6e, self.upsample_3x3(x7e)), 1))   # 3x3,      1024
         x5d = self.conv2d_01d(torch.cat((x5e, self.upsample_7x7(x6d)), 1))  # 7x7,      512
