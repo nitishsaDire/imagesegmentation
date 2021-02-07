2109e127e122bba30ebcd4ef48b08432d89cc823
nitishsadire <nitishsandhu9@gmail.com>
2021-02-07 13:39:27 +0530

    changes

--- Model.py
+++ Model.py
@@ -15,7 +15,6 @@ class UNET_resnet34(nn.Module):
         self.resnet = models.resnet34(pretrained=True)
         self.resnet_layers = list(self.resnet.children())
         self.upsample = nn.Upsample(scale_factor=2, mode='nearest')
-        self.upsample_3x3 = nn.Upsample((3,3), mode='nearest')
         self.upsample_7x7 = nn.Upsample((7,7), mode='nearest')
 
 
@@ -27,9 +26,11 @@ class UNET_resnet34(nn.Module):
 
         self.conv2d_0  = self.conv2d(512, 1024, kernel=3, stride = 2, padding=1)
         self.conv2d_01 = self.conv2d(1024, 2048, kernel=3, stride = 2, padding=1)
+        self.adaptivePooling = nn.AdaptiveAvgPool2d((1,1))
 
-        self.conv2d_0d = self.conv2d(2048 + 1024, 1024, kernel=3, padding=1)
-        self.conv2d_01d = self.conv2d(1024 + 512, 512, kernel=3, padding=1)
+        self.conv2d_00d = self.conv2d(2048 + 2048, 2048, kernel=3, padding=1)
+        self.conv2d_01d = self.conv2d(2048 + 1024, 1024, kernel=3, padding=1)
+        self.conv2d_02d = self.conv2d(1024 + 512, 512, kernel=3, padding=1)
         self.conv2d_1 = self.conv2d(512 + 256, 256, kernel=3, padding=1)
         self.conv2d_2 = self.conv2d(256 + 128, 128, kernel=3, padding=1)
         self.conv2d_3 = self.conv2d(128 + 64, 64, kernel=3, padding=1)
@@ -65,14 +66,17 @@ class UNET_resnet34(nn.Module):
         x3e = self.seq3(x2e)        # 28x28, 128
         x4e = self.seq4(x3e)        # 14x14, 256
         x5e = self.seq5(x4e)        # 7x7, 512
-        x6e = self.conv2d_0(x5e)    # 3x3, 1024
-        x7e = self.conv2d_01(x6e)   # 1x1, 2048
+        x6e = self.conv2d_0(x5e)    # 4x4, 1024
+        x7e = self.conv2d_01(x6e)   # 2x2, 2048
+        x8e = self.adaptivePooling(x7e)   # 1x1, 2048
         print(x5e.shape)
         print(x6e.shape)
         print(x7e.shape)
+        print(x8e.shape)
 
-        x6d = self.conv2d_0d(torch.cat((x6e, self.upsample_3x3(x7e)), 1))   # 3x3,      1024
-        x5d = self.conv2d_01d(torch.cat((x5e, self.upsample_7x7(x6d)), 1))  # 7x7,      512
+        x7d = self.conv2d_00d(torch.cat((x7e, self.upsample(x8e)), 1))      # 2x2,      2048
+        x6d = self.conv2d_01d(torch.cat((x6e, self.upsample(x7d)), 1))      # 3x3,      1024
+        x5d = self.conv2d_02d(torch.cat((x5e, self.upsample_7x7(x6d)), 1))  # 7x7,      512
         x4d = self.conv2d_1(torch.cat((x4e, self.upsample(x5d)), 1))        # 14x14,    256
         x3d = self.conv2d_2(torch.cat((x3e, self.upsample(x4d)), 1))        # 28x28,    128
         x2d = self.conv2d_3(torch.cat((x2e, self.upsample(x3d)), 1))        # 56x56,    64
