6131506b18fc46add79613cbb0e3910e0a47b4d1
nitishsadire <nitishsandhu9@gmail.com>
2021-02-09 14:09:45 +0530

    using both losses

--- Main.py
+++ Main.py
@@ -165,7 +165,8 @@ def train_model(unet, optimizer, scheduler, dataloader, dataset_sizes, device, l
                                         fig, ax = plt.subplots()
                                         plt.imshow(masks_to_colorimg(outputs[indexx].cpu()))
                                         plt.show()
-                                    loss = 0.66 * F.binary_cross_entropy_with_logits(outputs.to(device), mask.to(torch.float)) + 0.33 * dice_loss(outputs.to(device), mask.to(torch.float))
+                                    loss = 0.66 * F.binary_cross_entropy(outputs.to(device), mask.to(torch.float)) \
+                                         + 0.33 * dice_loss(outputs.to(device), mask.to(torch.float))
                                     print(0.66 * F.binary_cross_entropy_with_logits(outputs.to(device), mask.to(torch.float)), 0.33 * dice_loss(outputs.to(device), mask.to(torch.float)))
                                     # backward + optimize only if in training phase
                                     if phase == 'train':
