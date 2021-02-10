6312058a7c9162130608fe5ee81485e1d2c94b93
nitishsadire <nitishsandhu9@gmail.com>
2021-02-10 17:03:52 +0530

    different thresholds

--- Main.py
+++ Main.py
@@ -169,7 +169,8 @@ def train_model(unet, optimizer, scheduler, dataloader, dataset_sizes, device, l
                                         plt.show()
                                     loss =  0.7 * F.binary_cross_entropy_with_logits(outputs.to(device), mask.to(torch.float))\
                                             + 0.3 * dice_loss(torch.sigmoid(outputs.to(device)), mask.to(torch.float))
-                                    # print("tsm",0.66 * F.binary_cross_entropy_with_logits(outputs.to(device), mask.to(torch.float)), 0.33 * dice_loss(outputs.to(device), mask.to(torch.float)))
+                                    print("tsm", 0.7 * F.binary_cross_entropy_with_logits(outputs.to(device), mask.to(torch.float)),\
+                                             0.3 * dice_loss(torch.sigmoid(outputs.to(device)), mask.to(torch.float)))
                                     # backward + optimize only if in training phase
                                     if phase == 'train':
                                         loss.backward()
