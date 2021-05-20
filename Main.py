49a8d529f1cc638f3188babe35410dfaf8935b2f
nitishsadire <nitishsandhu9@gmail.com>
2021-05-20 10:51:17 +0530

    new paths

--- Main.py
+++ Main.py
@@ -81,8 +81,8 @@ def train_model(unet, optimizer, scheduler, dataloader, dataset_sizes, device, l
         epoch_losses[k] = []
         epoch_accuracies[k] = []
 
-    OLD_PATH = '/content/drive/MyDrive/sem_is_3_l'
-    PATH = '/content/drive/MyDrive/sem_is_4_l'
+    OLD_PATH = '/content/drive/MyDrive/sem_is_4_l'
+    PATH = '/content/drive/MyDrive/sem_is_5_l'
     epoch = 0
     if loadModel == True:
         checkpoint = torch.load(OLD_PATH)
